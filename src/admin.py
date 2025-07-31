c4f4bc29fdc0c1ca63554c43282b88269c32ec1d
Bryan Chau <bchau2020@fau.edu>
2025-07-31 00:56:45 +0000

    added frontend

--- src/admin.py
+++ src/admin.py
@@ -2,10 +2,18 @@ from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from models import db, User, Person, Planet, Favorite
 
+class FavoriteView(ModelView):
+    column_list = ["id", "user_id", "planet_id", "person_id"]
+    column_labels = {
+        "user_id": "User",
+        "planet_id": "Planet",
+        "person_id": "Person"
+    }
+
 def setup_admin(app):
     admin = Admin(app, name='StarWars Admin', template_mode='bootstrap3')
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Person, db.session))
     admin.add_view(ModelView(Planet, db.session))
-    admin.add_view(ModelView(Favorite, db.session))
+    admin.add_view(FavoriteView(Favorite, db.session))
     
